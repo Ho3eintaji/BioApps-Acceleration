13fe232325c1af4e8e7d3d8d99fcdcb8125ba406
Müller Christoph Thomas <christoph.mueller@epfl.ch>
2022-07-13 08:56:37 +0200

    added environment files, closes issue #2

---
+++ env.csh
@@ -0,0 +1,10 @@
+setenv MODEL_TECH /softs/mentor/qsta/2020.4/bin
+setenv MGLS_LICENSE_FILE 1717@ielsrv01.epfl.ch
+setenv PATH "/softs/mentor/qsta/2020.4/bin/:$PATH"
+setenv RISCV /shares/eslfiler1/apps/linux/Development/rv32imc/setenv 
+setenv MTI_VCO_MODE 64
+setenv XILINX_VIVADO "/softs/xilinx/vivado/2020.2_lin64/"
+setenv PATH "$XILINX_VIVADO/bin:/edadk_repo/softs/xilinx/DocNav:$PATH"
+setenv XILINXD_LICENSE_FILE 2100@ielsrv01.epfl.ch
+
+conda activate core-v-mini-mcu
---
+++ env.sh
@@ -0,0 +1,10 @@
+export MODEL_TECH=/softs/mentor/qsta/2020.4/bin
+export MGLS_LICENSE_FILE=1717@ielsrv01.epfl.ch
+export PATH="/softs/mentor/qsta/2020.4/bin/:$PATH"
+export RISCV=/shares/eslfiler1/apps/linux/Development/rv32imc/setenv 
+export MTI_VCO_MODE=64
+export XILINX_VIVADO="/softs/xilinx/vivado/2020.2_lin64/"
+export PATH="$XILINX_VIVADO/bin:/edadk_repo/softs/xilinx/DocNav:$PATH"
+export XILINXD_LICENSE_FILE=2100@ielsrv01.epfl.ch
+
+conda activate core-v-mini-mcu
