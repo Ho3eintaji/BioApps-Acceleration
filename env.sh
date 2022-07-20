d19868d361301f95583531afddc2480e77593255
Davide Schiavone <davide@openhwgroup.org>
2022-07-20 10:02:15 +0200

    fix env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 export MODEL_TECH=/softs/mentor/qsta/2020.4/bin
 export MGLS_LICENSE_FILE=1717@ielsrv01.epfl.ch
 export PATH="/softs/mentor/qsta/2020.4/bin/:$PATH"
-export RISCV=/shares/eslfiler1/apps/linux/Development/rv32imc/setenv 
+export RISCV=/shares/eslfiler1/apps/linux/Development/rv32imc
 export MTI_VCO_MODE=64
 export XILINX_VIVADO="/softs/xilinx/vivado/2020.2_lin64/"
 export PATH="$XILINX_VIVADO/bin:/edadk_repo/softs/xilinx/DocNav:$PATH"
