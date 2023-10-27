ec14b341156015c41717c4130e0d50518bcc071d
Taji Hossein <taji@eslpc2.intranet.epfl.ch>
2023-10-27 16:39:02 +0200

    doing all the measurements on the sleep and deep sleep modes

---
+++ sw/applications/example_dp_slp_mode/ene_modes.c
@@ -0,0 +1,26 @@
+/*
+ * ene_modes.c
+ * Author: Hossein Taji
+ * Date: Oct, 2023
+ * Version: 1.0    
+ * Description: source file of the ene_modes.h
+ */
+
+
+#include "ene_modes.h"
+
+
+void init_ene_modes(uint64_t sys_freq){
+    /* setup power manager */
+
+    /* 
+
+}
+
+void en_sleep_mode(){
+
+}
+
+void en_deep_sleep_mode(){
+
+}
--- sw/applications/example_dp_slp_mode/example_dp_slp_mode.c
+++ sw/applications/example_dp_slp_mode/example_dp_slp_mode.c
@@ -19,21 +19,25 @@
 #include "hart.h"
 #include "handler.h"
 
-
-
-#define SYS_FREQ        150*1000000 //MHz
-#define TIMER_FREQ      0.1*1000000 // 100 KHz
 #define PIN_TRIGGER     4  //used for trigering and checking on oscilloscope
 
 // #define PUT_SLEEP
 #define PUT_DEEP_SLEEP
 // #define PUT_IDLE
 
-
+uint32_t fll_freq, fll_freq_real;
+fll_t fll;
+soc_ctrl_t soc_ctrl;
+uint32_t fll_status;
+uint32_t config_fll_ol_low;
+uint32_t config_fll_ol_high;
+const uint64_t TIMER_FREQ = 0.1*1000000; // 100 KHz
+const uint64_t SYS_FREQ = 150*1000000; //MHz
 gpio_t gpio;
+rv_timer_t timer_0_1;
+
 static power_manager_t power_manager;
 
-static rv_timer_t timer_0_1;
 
 int main(int argc, char const *argv[])
 {
@@ -62,12 +66,9 @@ int main(int argc, char const *argv[])
      * ============
     */ 
     // 2.1 FLL peripheral structure to access the registers
-    uint32_t fll_freq, fll_freq_real;
-    fll_t fll;
     fll.base_addr = mmio_region_from_addr((uintptr_t)FLL_START_ADDRESS);
     fll_init(&fll);
-    uint32_t fll_status = fll_status_get(&fll);
-    soc_ctrl_t soc_ctrl;
+    fll_status = fll_status_get(&fll);
     soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
 
     // 2.2. Enable open loop mode :)
@@ -77,14 +78,14 @@ int main(int argc, char const *argv[])
 
 
     // 2.3 Values for openloop switching
-    const uint32_t config_fll_ol_low = fll_create_config_1((fll_conf1_reg_t){
+    config_fll_ol_low = fll_create_config_1((fll_conf1_reg_t){
         .mult_factor = fll_conf1.mult_factor,
         .dco_input   = 170,
         .clk_div     = 0x2,
         .lock_enable = fll_conf1.lock_enable,
         .op_mode     = fll_conf1.op_mode
     });
-    const uint32_t config_fll_ol_high = fll_create_config_1((fll_conf1_reg_t){
+    config_fll_ol_high = fll_create_config_1((fll_conf1_reg_t){
         .mult_factor = fll_conf1.mult_factor,
         .dco_input   = 807,
         .clk_div     = 0x2,
@@ -297,7 +298,6 @@ int main(int argc, char const *argv[])
      * == Power Gate =====
      * ===================
     */
-    gpio_write(&gpio, PIN_TRIGGER, true);
     power_gate_periph(&power_manager, kOff_e, &power_manager_counters); // 1.1. Power gating peripheral domain
     power_gate_ram_block(&power_manager, 2, kOff_e, &power_manager_counters); // 1.2. Power gating memory banks
     power_gate_ram_block(&power_manager, 3, kOff_e, &power_manager_counters);
@@ -308,34 +308,34 @@ int main(int argc, char const *argv[])
     power_gate_external(&power_manager, 0, kOff_e, &power_manager_counters); // 1.3.1. Power gating CGRA logic
     power_gate_external(&power_manager, 1, kOff_e, &power_manager_counters); // 1.3.2. Power gating CGRA memories
     power_gate_external(&power_manager, 2, kOff_e, &power_manager_counters); // 1.3.3. Power gating Coubertin
-    /*
-     * =====================
-     * == FLL, Low Freq ====
-     * =====================
-    */
-    fll_conf1_set(&fll, config_fll_ol_low);     //open loop
-    fll_freq = fll_get_freq(&fll);
-    soc_ctrl_set_frequency(&soc_ctrl, fll_freq);
+    // /*
+    //  * =====================
+    //  * == FLL, Low Freq ====
+    //  * =====================
+    // */
+    // fll_conf1_set(&fll, config_fll_ol_low);     //open loop
+    // fll_freq = fll_get_freq(&fll);
+    // soc_ctrl_set_frequency(&soc_ctrl, fll_freq);
     /*
      * =======================
      * == Power Gate Core ====
      * =======================
     */
-    rv_timer_arm(&timer_0_1, 0, 0, 3000);   //changing timer parameter because frequency has changed(?!)
-    rv_timer_counter_set_enabled(&timer_0_1, 0, kRvTimerEnabled); //start timer here
+    // rv_timer_arm(&timer_0_1, 0, 0, 3000);   //changing timer parameter because frequency has changed(?!)
+    // rv_timer_counter_set_enabled(&timer_0_1, 0, kRvTimerEnabled); //start timer here
     CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
-    power_gate_core(&power_manager, kTimer_0_pm_e, &power_manager_counters_cpu); //todo: remove
+    power_gate_core_dpslp(&power_manager, kTimer_0_pm_e, &power_manager_counters_cpu); //todo: remove
     CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
     // gpio_write(&gpio, PIN_TRIGGER, false);
-    /*
-     * =====================
-     * == FLL, High Freq ===
-     * =====================
-    */
-    fll_conf1_set(&fll, config_fll_ol_high);    // open loop step
-    fll_set_freq(&fll, SYS_FREQ);               // feedback step
-    fll_freq_real = fll_get_freq(&fll);
-    soc_ctrl_set_frequency(&soc_ctrl, fll_freq_real);
+    // /*
+    //  * =====================
+    //  * == FLL, High Freq ===
+    //  * =====================
+    // */
+    // fll_conf1_set(&fll, config_fll_ol_high);    // open loop step
+    // fll_set_freq(&fll, SYS_FREQ);               // feedback step
+    // fll_freq_real = fll_get_freq(&fll);
+    // soc_ctrl_set_frequency(&soc_ctrl, fll_freq_real);
     /*
      * ===================
      * == Power On =======
@@ -351,6 +351,7 @@ int main(int argc, char const *argv[])
     power_gate_external(&power_manager, 0, kOn_e, &power_manager_counters); // 1.3.1. Power on CGRA logic
     power_gate_external(&power_manager, 1, kOn_e, &power_manager_counters); // 1.3.2. Power on CGRA memories
     power_gate_external(&power_manager, 2, kOn_e, &power_manager_counters); // 1.3.3. Power on Coubertin
+    gpio_write(&gpio, PIN_TRIGGER, false);
     /*
      * ============
      * == App =====
